eac2bc5b0b5536a9f9e12397af0013530d6fdae9
arun <arun.kirubarajan@gmail.com>
2018-02-05 21:57:52 -0500

    Add body-parser and refactor routes

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const body_parser = require('body-parser');
+const env = require('dotenv');
+const homepage_routes = require('./routes/homepage');
 const app = express();
-require('dotenv').config();
+env.config();
+
+app.use(body_parser.urlencoded({extended: true}));
+app.use(body_parser.json());
 
 app.get('/api', (req, res) => {
   res.send("Welcome to the API!");
 });
 
-app.listen(process.env.PORT, err => {
-  if (err) {
-    console.error(err);
-  }
+app.use('/api/homepage', homepage_routes);
 
+app.listen(process.env.PORT, err => {
   console.log('Listening on port ' + process.env.PORT);
-})
+});
